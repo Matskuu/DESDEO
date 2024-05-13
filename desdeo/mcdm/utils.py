30d562115d3e2a4f0d6751fcc2f0999045e39c4e
Matias Nieminen <84133647+Matskuu@users.noreply.github.com>
2024-05-13 10:39:29 +0300

    Added choosing scalarization function and the arguments for that as optional parameters.

--- desdeo/mcdm/utils.py
+++ desdeo/mcdm/utils.py
@@ -1,6 +1,10 @@
 
+import numpy as np
+from typing import Callable
 from desdeo.problem import (
+    GenericEvaluator,
     Problem,
+    dtlz2,
     pareto_navigator_test_problem
 )
 from desdeo.tools.generics import CreateSolverType, SolverResults
@@ -10,15 +14,17 @@ from desdeo.tools.scalarization import (
     add_scalarization_function
 )
 from desdeo.tools.utils import guess_best_solver
+from desdeo.tools.pyomo_solver_interfaces import PyomoIpoptSolver
 
 def project(
     problem: Problem,
     approximated_solution: dict[str, float],
-    scalarization_function: str | None = None,
-    create_solver: CreateSolverType | None = None
+    create_solver: CreateSolverType | None = None,
+    scalarization_function: Callable | None = None,
+    args: list | None = None
 ) -> SolverResults:
     if scalarization_function:
-        problem_with_scalarization, target = add_scalarization_function(problem, scalarization_function, "target")
+        problem_with_scalarization, target = scalarization_function(*args)
     else:
         problem_with_scalarization, target = add_asf_nondiff(problem, "target", approximated_solution)
 
@@ -27,15 +33,24 @@ def project(
     return solver.solve(target)
 
 if __name__ == "__main__":
-    problem = pareto_navigator_test_problem()
-    reference_point = {"f_1": 0.35, "f_2": -0.51, "f_3": -26.26}
-    res = project(problem, reference_point)
-    print(reference_point, res.optimal_objectives)
+    #problem = pareto_navigator_test_problem()
+    problem = dtlz2(8, 3)
 
-    reference_point = {"f_1": -0.89, "f_2": 2.91, "f_3": -24.98}
-    res = project(problem, reference_point)
-    print(reference_point, res.optimal_objectives)
+    #xs = {f"{var.symbol}": [0.5] for var in problem.variables}
+    #evaluator = GenericEvaluator(problem)
+    #res = evaluator.evaluate(xs)
+    #reference_point = {f"{obj.symbol}": res[obj.symbol][0] for obj in problem.objectives}
 
-    reference_point = {"f_1": -0.32, "f_2": 2.33, "f_3": -27.85}
-    res = project(problem, reference_point)
-    print(reference_point, res.optimal_objectives)
+    reference_point = {"f_1": 0.4, "f_2": 0.7, "f_3": 0.5}
+    #args = [problem, "target", reference_point]
+    #print(args)
+    #res = project(problem, reference_point, add_asf_nondiff, args)
+
+    res = project(problem, reference_point, PyomoIpoptSolver)
+    """
+    print(reference_point, res.optimal_objectives, res.optimal_variables)
+    values_sq = 0
+    for obj in problem.objectives:
+        values_sq += res.optimal_objectives[obj.symbol]
+    print(values_sq)
+    """
